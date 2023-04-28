ceffa0565e705508928d9c18ece03b108e05c8e7
Jaime Alberto López Restrepo <lopezjaime433@gmail.com>
2023-04-28 12:09:26 -0500

    Update style in views login activity and register

--- app/src/main/java/com/example/startup/LoginActivity.kt
+++ app/src/main/java/com/example/startup/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.EditText
 import android.widget.GridLayout
 import android.widget.TextView
@@ -14,7 +13,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
-
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

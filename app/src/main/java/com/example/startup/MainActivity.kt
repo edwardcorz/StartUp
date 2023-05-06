e32117a5065bfba9f8b10cd22e3ac921801bd15c
tephaMR22 <stephania.martinez.rodrigurz@correounivalle.edu.co>
2023-05-06 11:50:28 -0500

    update

--- app/src/main/java/com/example/startup/MainActivity.kt
+++ app/src/main/java/com/example/startup/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.startup
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,7 +13,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,7 +29,15 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.colorPrimaryDark)
+        }
+
         supportActionBar?.hide()
+
         navView.setupWithNavController(navController)
     }
 }

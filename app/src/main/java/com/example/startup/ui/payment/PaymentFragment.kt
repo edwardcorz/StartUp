293282beaa87c14fd300c529d5eb7c08ed5c8eb0
Jaime Alberto López Restrepo <lopezjaime433@gmail.com>
2023-05-12 14:51:15 -0500

    Update

--- app/src/main/java/com/example/startup/ui/payment/PaymentFragment.kt
+++ app/src/main/java/com/example/startup/ui/payment/PaymentFragment.kt
@@ -1,14 +1,17 @@
 package com.example.startup.ui.payment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.startup.R
 import com.example.startup.conexionBD
 import com.example.startup.databinding.FragmentPaymentBinding
+import com.stripe.android.Stripe
+
 
 class PaymentFragment : Fragment() {
 
@@ -25,6 +28,8 @@ class PaymentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
+        val stripe = Stripe(requireContext(), "pk_test_51N4ntfFMwr78vVxVjQ0xXvsz1tDzqAsWlT3TpxRdkhBq1zL7ryrh767kO9xbqKecVLqd8fOn7zRiAaRL6fkmciWV00R5Wh852w")
+
         _binding = FragmentPaymentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
@@ -33,6 +38,8 @@ class PaymentFragment : Fragment() {
         conexion.conexionNombre(textView)
 
         return root
+
+        var buttonPagar = root.findViewById<Button>(R.id.button)
     }
 
     override fun onDestroyView() {

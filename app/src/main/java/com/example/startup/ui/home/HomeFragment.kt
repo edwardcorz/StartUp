a0a563817fd34e413986f033c583416aa52c4bd2
Jaime Alberto López Restrepo <lopezjaime433@gmail.com>
2023-05-13 10:50:27 -0500

    Update

--- app/src/main/java/com/example/startup/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/startup/ui/home/HomeFragment.kt
@@ -17,54 +17,6 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
 
-//class HomeFragment : Fragment() {
-//    private val conexion =conexionBD()
-//    private var _binding: FragmentHomeBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//        // Crea el gráfico de barras
-//        val chart = root.findViewById<BarChart>(R.id.chart)
-//
-//        val entries = listOf(
-//            BarEntry(0f, 4f),
-//            BarEntry(1f, 6f),
-//            BarEntry(2f, 8f),
-//            BarEntry(3f, 7f),
-//            BarEntry(4f, 1f),
-//            BarEntry(5f, 8f),
-//            BarEntry(6f, 3f),
-//            BarEntry(7f, 5f),
-//            BarEntry(8f, 8f),
-//            BarEntry(9f, 2f)
-//        )
-//
-//        val dataSet = BarDataSet(entries, "Resumen de entreno")
-//        dataSet.color = Color.rgb(120, 16, 2)
-//
-//        val data = BarData(dataSet)
-//        data.barWidth = 0.3f
-//
-//        chart.data = data
-//        chart.setFitBars(true)
-//        chart.animateY(1500)
-//        chart.description.textColor = Color.BLACK
-//        chart.description.textSize = 14f
-//        chart.legend.isEnabled = false
-//        chart.description.text = ""
-//        chart.axisLeft.axisMinimum = 0f
-//        chart.axisRight.isEnabled = false
-//        chart.xAxis.isEnabled = false
-
 class HomeFragment : Fragment() {
     private val conexion =conexionBD()
     private var _binding: FragmentHomeBinding? = null
@@ -85,16 +37,18 @@ class HomeFragment : Fragment() {
         val chart = root.findViewById<BarChart>(R.id.chart)
 
         val entries = listOf(
-            BarEntry(0f, 4f),
-            BarEntry(1f, 6f),
-            BarEntry(2f, 8f),
-            BarEntry(3f, 7f),
-            BarEntry(4f, 1f),
-            BarEntry(5f, 8f),
-            BarEntry(6f, 3f),
-            BarEntry(7f, 5f),
-            BarEntry(8f, 8f),
-            BarEntry(9f, 2f)
+            BarEntry(0f, 12f),
+            BarEntry(1f, 23f),
+            BarEntry(2f, 14f),
+            BarEntry(3f, 15f),
+            BarEntry(4f, 21f),
+            BarEntry(5f, 0f),
+            BarEntry(6f, 0f),
+            BarEntry(7f, 0f),
+            BarEntry(8f, 0f),
+            BarEntry(9f, 0f),
+            BarEntry(10f, 0f),
+            BarEntry(11f,0f)
         )
 
         val dataSet = BarDataSet(entries, "Resumen de entrenamiento")
@@ -154,10 +108,6 @@ class HomeFragment : Fragment() {
         conexion.conexionNombre(textView)
         conexion.extraerPlan(textViewPlan)
         conexion.cargarFoto(requireContext(),root)
-
-        chart.axisLeft.isEnabled = false
-        chart.setVisibleXRangeMaximum(8F)
-        chart.moveViewToX(1F)
 
         return root
     }
